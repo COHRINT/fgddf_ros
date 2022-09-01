Fixed random changes and modified noise calculation

circle.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import Twist
 
 class Circle:
         def __init__(self):
-            self.pub = rospy.Publisher("/tars/jackal_velocity_controller/cmd_vel",Twist,queue_size=10)
+            self.pub = rospy.Publisher("/case/jackal_velocity_controller/cmd_vel",Twist,queue_size=10)
             self.linear_speed = 0.3
             # self.diameter = 2
             # self.angular_speed = -1 * (np.log(self.diameter / 3.634) / -3.036)
@@ -20,5 +20,5 @@ class Circle:
                 rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node("circle")
+    rospy.init_node("circle_case")
     c = Circle()

main.py
@@ -84,7 +84,7 @@ DEBUG = 0
 dt = 0.1
 nAgents = 2   # number of agents
 saveFlag = 1
-conservativeFlag =  1 # use conservative marginalization
+conservativeFlag =  0 # use conservative marginalization
 YData = dict()
 fusionFlag = 0 # start fusing from time stp 5
 #N = 1000 # Time steps
@@ -431,7 +431,7 @@ while not rospy.is_shutdown() and (k < 200):
             ag["agent"] = mergeFactors(ag["agent"], ag["agent"].fusion.commonVars[key])
 
         ag["agent"].build_semiclique_tree()
-        tmpGraph = dict()
+        # tmpGraph = dict()
 
         # inference
         jointInfMat=buildJointMatrix(ag['agent'])
@@ -442,12 +442,12 @@ while not rospy.is_shutdown() and (k < 200):
 
     agents = inferState(agents, dynamicList, nAgents, m,  firstRunFlag, saveFlag = 1)
 
-    del tmpGraph
+    # del tmpGraph
     k += 1
 
     # Save data from current time step
     for var in ag["agent"].varSet:
-        print("Data logging debuggin:")
+        # print("Data logging debuggin:")
         ag_tag = "X" + str(ag_idx + 1)
         if var != ag_tag:
             agent_results = Results()
@@ -464,17 +464,17 @@ while not rospy.is_shutdown() and (k < 200):
             agent_results.FullCovDim = np.array(ag["results"][0]["FullCov"].shape)
             agent_results.FullCov = ag["results"][0]["FullCov"].flatten()
 
-            print("Target data")
-            print(ag["results"][0][var + "_mu"])
-            print(ag["results"][0][var + "_cov"])
+            # print("Target data")
+            # print(ag["results"][0][var + "_mu"])
+            # print(ag["results"][0][var + "_cov"])
             agent_results.TMuDim = np.array(ag["results"][0][var + "_mu"].shape)
             agent_results.TMu = ag["results"][0][var + "_mu"].flatten()
             agent_results.TCovDim = np.array(ag["results"][0][var + "_cov"].shape)
             agent_results.TCov = ag["results"][0][var +"_cov"].flatten()
 
-            print("agent data")
-            print(ag["results"][0][ag_tag + "_mu"])
-            print(ag["results"][0][ag_tag + "_cov"])
+            # print("agent data")
+            # print(ag["results"][0][ag_tag + "_mu"])
+            # print(ag["results"][0][ag_tag + "_cov"])
             agent_results.SMuDim = np.array(ag["results"][0][ag_tag + "_mu"].shape)
             agent_results.SMu = ag["results"][0][ag_tag + "_mu"].flatten()
             agent_results.SCovDim = np.array(ag["results"][0][ag_tag + "_cov"].shape)

src/fgDDF/FG_KF.py
@@ -506,7 +506,7 @@ class FG_EKF(FG_KF):
 				X=getattr(self, var)
 				u=np.reshape(X['u'][:, predNum-1], (len(X['u']), 1))
 				F = X['dynJacobian'](x_m, u, dt)
-				sol = solve_ivp(lambda t, x_m: X['dynFxn'](t, x_m, u, 0.6), (0, dt), x_m.reshape(-1), method = 'RK45', t_eval = None, rtol=1e-9, atol=1e-12)
+				sol = solve_ivp(lambda t, x_m: X['dynFxn'](t, x_m, u, 0.4), (0, dt), x_m.reshape(-1), method = 'RK45', t_eval = None, rtol=1e-9, atol=1e-12)
 
 				fx = sol.y[:,-1].reshape((sol.y.shape[0],1))
 				Qinv=X['Qinv']

src/fgDDF/inputFile.py
@@ -29,6 +29,7 @@ nTargets = 1   # number of targets
 nLM = 4      #number of landmarks
 
 matFile = sio.loadmat("catkin_ws/src/fgddf_ros/src/fgDDF/trackingAndLocalization_2A_1T_MC.mat")
+print(matFile)
 
 N = matFile['tVec'].shape[1]
 
@@ -181,7 +182,7 @@ for t in range(1, nTargets+1):
     variables["T"+str(t)]["u"] = vector(matFile['vT'][t-1].item(), matFile['w_t'][t-1].item())*np.ones((1,N), dtype=np.float64)
 
 # Agent name
-agent_name = "cohrint_tars"
+agent_name = "cohrint_case"
 
 # Target names
 target1 = "cohrint_tycho_bot_1"

src/fgDDF/truthFxn.py
@@ -21,7 +21,7 @@ def truthRangeMeas(rf, current_agent, agent_idx, target_idx, is_target):
     else:
         x2 = rf.landmark_pos[target_idx]
 
-    noise = np.random.normal(0,current_agent["R"])
+    noise = np.random.normal(0,np.sqrt(current_agent["R"]))
 
     y = math.dist(x1[0:2],x2)
     y = y + noise
@@ -40,7 +40,7 @@ def truthAzimuthMeas(rf, current_agent, agent_idx, target_idx, is_target):
     else:
         x2 = rf.landmark_pos[target_idx]
 
-    noise = np.random.normal(0,current_agent["R"])
+    noise = np.random.normal(0,np.sqrt(current_agent["R"]))
 
     y = math.atan2(x2[1]-x1[1], x2[0]-x1[0])
     y = y + noise
@@ -65,7 +65,7 @@ def truthRelativeAzimuthMeas(rf, current_agent, agent_idx, target_idx, is_target
         # print(rf.landmark_pos)
         x2 = rf.landmark_pos[target_idx]
 
-    noise = np.random.normal(0,current_agent["R"])
+    noise = np.random.normal(0,np.sqrt(current_agent["R"]))
 
     y = math.atan2(x2[1]-x1[1], x2[0]-x1[0])-x1[2]
     y = y + noise