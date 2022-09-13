f0171afe2796cc0143b8733355b0391d9b2e3a0b
cohrint <cohrint@tars.cpr-unassigned>
2022-09-13 12:51:38 -0400

    added circle.py

---
+++ circle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import numpy as np
+import rospy
+from geometry_msgs.msg import Twist
+
+class Circle:
+        def __init__(self,name):
+            self.name = name
+
+            self.pub = rospy.Publisher("/"+self.name+"/jackal_velocity_controller/cmd_vel",Twist,queue_size=10)
+            self.linear_speed = 0.3
+            # self.diameter = 2
+            # self.angular_speed = -1 * (np.log(self.diameter / 3.634) / -3.036)
+            self.angular_speed = 0.3
+            rate = rospy.Rate(10)
+
+            while not rospy.is_shutdown():
+                t = Twist()
+                t.linear.x = self.linear_speed
+                t.angular.z = self.angular_speed
+                self.pub.publish(t)
+                rate.sleep()
+
+if __name__ == '__main__':
+    name = "tars"
+    rospy.init_node("circle_"+name)
+    c = Circle(name)
